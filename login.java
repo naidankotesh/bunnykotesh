ddf8b08d874e2362b2588a1db68f21731ed69d3c
root <root@ip-172-31-45-193.eu-north-1.compute.internal>
2024-07-11 03:32:45 +0000

    modified the code

--- login.java
+++ login.java
@@ -1,1 +1,16 @@
-kdaskjdakfak 
+Public class login
+{
+   public static void main()
+   {
+       int i =0;
+
+
+       for (i=0;i<==70;i++
+
+       
+
+       {
+            System.out Println ("Number: "+i);
+       }
+   }
+} 
